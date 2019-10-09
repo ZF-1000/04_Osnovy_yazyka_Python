b9f09b1343988eed78e11ef297af6a4b8acd15ae
ZF-1000 <n.shanin@mail.ru>
2019-10-09 21:23:27 +0300

    Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс. Используйте форматирование строк

---
+++ DZ_01_task_02.py
@@ -0,0 +1,14 @@
+"""Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и
+выведите в формате чч:мм:сс. Используйте форматирование строк."""
+
+seconds = input("Введите количество секунд для перевода в формат <<hour : min : sec>>:\n")
+while not seconds.isdigit():
+    seconds = input("Пожалуйста, введите целое положительное число: ")
+else:
+    seconds = int(seconds)
+    hours = seconds // 3600
+    seconds = seconds % 3600
+    minutes = seconds // 60
+    seconds = seconds % 60
+    print(f"{hours}:{minutes}:{seconds}")
+input()
